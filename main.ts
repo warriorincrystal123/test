b5ecf182b37a1a08ca2055e125e684bcf035d2df
warriorincrystal123 <130286520+warriorincrystal123@users.noreply.github.com>
2023-04-19 21:06:08 +0800

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -9,17 +9,20 @@ loops.everyInterval(1000, function () {
     timeanddate.advanceBy(1, timeanddate.TimeUnit.Seconds)
 })
 basic.forever(function () {
-    if (pins.digitalReadPin(DigitalPin.P2) == 1 && timeanddate.time(timeanddate.TimeFormat.HMMSSAMPM) == "09:05.00") {
-        radio.sendString("meds weren't taken")
-        basic.showIcon(IconNames.No)
-        basic.pause(1000)
-        basic.clearScreen()
+    while (pins.digitalReadPin(DigitalPin.P2) == 1 && (timeanddate.time(timeanddate.TimeFormat.HHMMSS24hr) >= "09:00.00" && timeanddate.time(timeanddate.TimeFormat.HHMMSS24hr) <= "09:05.00")) {
+        music.playMelody("C C5 C C5 C C5 C C5 ", 250)
     }
 })
 basic.forever(function () {
     if (pins.digitalReadPin(DigitalPin.P2) == 0 && (timeanddate.time(timeanddate.TimeFormat.HHMMSS24hr) >= "09:00.00" && timeanddate.time(timeanddate.TimeFormat.HHMMSS24hr) <= "09:05.00")) {
         basic.showIcon(IconNames.Yes)
         music.stopAllSounds()
+    }
+    if (pins.digitalReadPin(DigitalPin.P2) == 1 && timeanddate.time(timeanddate.TimeFormat.HMMSSAMPM) == "09:05.00") {
+        radio.sendString("meds weren't taken")
+        basic.showIcon(IconNames.No)
+    }
+    if (timeanddate.time(timeanddate.TimeFormat.HMMSSAMPM) == "09:05.30") {
         basic.pause(1000)
         basic.clearScreen()
     }
@@ -27,10 +30,5 @@ basic.forever(function () {
 basic.forever(function () {
     if (timeanddate.time(timeanddate.TimeFormat.HHMMSS24hr) == "09:00.00") {
         radio.sendString("take your meds")
-    }
-})
-basic.forever(function () {
-    while (pins.digitalReadPin(DigitalPin.P2) == 1 && (timeanddate.time(timeanddate.TimeFormat.HHMMSS24hr) >= "09:00.00" && timeanddate.time(timeanddate.TimeFormat.HHMMSS24hr) <= "09:05.00")) {
-        music.playMelody("C C5 C C5 C C5 C C5 ", 250)
     }
 })
