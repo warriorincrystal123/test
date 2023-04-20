a1e66b688f6b93125358c04a105230666dff92c7
warriorincrystal123 <130286520+warriorincrystal123@users.noreply.github.com>
2023-04-20 15:56:42 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,23 +2,23 @@ input.onButtonPressed(Button.B, function () {
     basic.showString(timeanddate.time(timeanddate.TimeFormat.HMMAMPM))
 })
 timeanddate.setTime(8, 59, 55, timeanddate.MornNight.AM)
-pins.setAudioPin(AnalogPin.P1)
+pins.setAudioPin(AnalogPin.P2)
 music.setVolume(255)
 radio.setGroup(218)
 loops.everyInterval(1000, function () {
     timeanddate.advanceBy(1, timeanddate.TimeUnit.Seconds)
 })
 basic.forever(function () {
-    while (pins.digitalReadPin(DigitalPin.P2) == 1 && (timeanddate.time(timeanddate.TimeFormat.HHMMSS24hr) >= "09:00.00" && timeanddate.time(timeanddate.TimeFormat.HHMMSS24hr) <= "09:05.00")) {
+    while (pins.digitalReadPin(DigitalPin.P1) == 1 && (timeanddate.time(timeanddate.TimeFormat.HHMMSS24hr) >= "09:00.00" && timeanddate.time(timeanddate.TimeFormat.HHMMSS24hr) <= "09:05.00")) {
         music.playMelody("C C5 C C5 C C5 C C5 ", 250)
     }
 })
 basic.forever(function () {
-    if (pins.digitalReadPin(DigitalPin.P2) == 0 && (timeanddate.time(timeanddate.TimeFormat.HHMMSS24hr) >= "09:00.00" && timeanddate.time(timeanddate.TimeFormat.HHMMSS24hr) <= "09:05.00")) {
+    if (pins.digitalReadPin(DigitalPin.P1) == 0 && (timeanddate.time(timeanddate.TimeFormat.HHMMSS24hr) >= "09:00.00" && timeanddate.time(timeanddate.TimeFormat.HHMMSS24hr) <= "09:05.00")) {
         basic.showIcon(IconNames.Yes)
         music.stopAllSounds()
     }
-    if (pins.digitalReadPin(DigitalPin.P2) == 1 && timeanddate.time(timeanddate.TimeFormat.HMMSSAMPM) == "09:05.00") {
+    if (pins.digitalReadPin(DigitalPin.P1) == 1 && timeanddate.time(timeanddate.TimeFormat.HMMSSAMPM) == "09:05.00") {
         radio.sendString("meds weren't taken")
         basic.showIcon(IconNames.No)
     }
